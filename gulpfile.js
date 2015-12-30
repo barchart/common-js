4fee16e64eddbf8a58f0035eeb44bc1ae9826ec9
Bryan Ingle <bryan.ingle@barchart.com>
2015-12-30 13:29:28 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,144 @@
+var gulp = require('gulp');
+
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var gitModified = require('gulp-gitmodified');
+var glob = require('glob');
+var jasmine = require('gulp-jasmine');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var source = require('vinyl-source-stream');
+var transform = require('vinyl-transform');
+var uglify = require('gulp-uglify');
+var util = require('gulp-util');
+
+var fs = require('fs');
+
+function getVersionFromPackage() {
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+}
+
+function getVersionForComponent() {
+    return getVersionFromPackage().split('.').slice(0, 2).join('.');
+}
+
+gulp.task('ensure-clean-working-directory', function() {
+    return gulp.src('./**/*')
+        .pipe(gitModified('M', 'A', 'D', 'R', 'C', 'U', '??'))
+        .on('data', function (file) {
+            if (file) {
+                throw new Error('Unable to proceed, your working directory is not clean.');
+            }
+        });
+});
+
+gulp.task('bump-version', function () {
+    return gulp.src([ './package.json' ])
+        .pipe(bump({ type: 'patch' }).on('error', util.log))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+    return gulp.src([ './', './dist/' ])
+        .pipe(git.add())
+        .pipe(gitModified('M', 'A'))
+        .pipe(git.commit('Release. Bump version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+    git.push('origin', 'master', cb);
+});
+
+gulp.task('create-tag', function (cb) {
+    var version = getVersionFromPackage();
+
+    git.tag(version, 'Release ' + version, function (error) {
+        if (error) {
+            return cb(error);
+        }
+
+        git.push('origin', 'master', { args: '--tags' }, cb);
+    });
+});
+
+gulp.task('build-browser', function() {
+    return browserify('./lib/index.js', { standalone: 'Barchart' }).bundle()
+        .pipe(source('barchart-common-' + getVersionForComponent() + '.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('./dist'))
+        .pipe(uglify())
+        .pipe(rename('barchart-common-' + getVersionForComponent() + '-min.js'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build-browser-tests', function () {
+    return browserify({ entries: glob.sync('test/specs/**/*.js') }).bundle()
+        .pipe(source('barchart-common-tests-' + getVersionForComponent() + '.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('test/dist'));
+});
+
+gulp.task('build-browser-components', [ 'build-browser' ]);
+
+gulp.task('build', [ 'build-browser-components' ]);
+
+gulp.task('execute-browser-tests', function () {
+    return gulp.src('test/dist/barchart-common-tests-' + getVersionForComponent() + '.js')
+        .pipe(jasmine());
+});
+
+gulp.task('execute-node-tests', function () {
+    return gulp.src(['lib/index.js', 'test/specs/**/*.js'])
+        .pipe(jasmine());
+});
+
+gulp.task('execute-tests', function (callback) {
+    runSequence(
+        'build-browser-tests',
+        'execute-browser-tests',
+        'execute-node-tests',
+
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            }
+
+            callback(error);
+        });
+});
+
+gulp.task('release', function (callback) {
+    runSequence(
+        'ensure-clean-working-directory',
+        'build',
+        'build-browser-tests',
+        'execute-browser-tests',
+        'execute-node-tests',
+        'bump-version',
+        'commit-changes',
+        'push-changes',
+        'create-tag',
+
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            } else {
+                console.log('Release complete');
+            }
+
+            callback(error);
+        });
+});
+gulp
+gulp.task('lint', function() {
+    return gulp.src([ './lib/**/*.js', './test/specs/**/*.js' ])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', [ 'execute-tests' ]);
+
+gulp.task('default', [ 'lint' ]);
---
+++ test/specs/common/collections/sorting/ComparatorBuilderSpec.js
@@ -0,0 +1,81 @@
+var ComparatorBuilder = require('../../../../../lib/common/collections/sorting/ComparatorBuilder');
+
+describe('When a ComparatorBuilder is composed with two comparators', function() {
+    'use strict';
+
+    var comparatorBuilder;
+
+    var comparatorOne;
+    var comparatorTwo;
+
+    var first = { x: 0, y: 0, toString: function() { return '[first]'; } };
+    var second = { x: 1, y: 0, toString: function() { return '[second]'; } };
+    var third = { x: 1, y: 1, toString: function() { return '[third]'; } };
+
+    beforeEach(function() {
+        comparatorOne = jasmine.createSpy('comparatorOne').and.callFake(function(a, b) {
+            return a.x - b.x;
+        });
+
+        comparatorTwo = jasmine.createSpy('comparatorTwo').and.callFake(function(a, b) {
+            return a.y - b.y;
+        });
+
+        comparatorBuilder = ComparatorBuilder.startWith(comparatorOne)
+            .thenBy(comparatorTwo);
+    });
+
+    describe('and the ComparatorBuilder sorts an array (which requires both comparators)', function() {
+        var arrayToSort;
+
+        beforeEach(function() {
+            arrayToSort = [ third, first, second ];
+
+            arrayToSort.sort(comparatorBuilder.toComparator());
+        });
+
+        it('the first comparator should be invoked', function() {
+            expect(comparatorOne).toHaveBeenCalled();
+        });
+
+        it('the second comparator should be invoked', function() {
+            expect(comparatorTwo).toHaveBeenCalled();
+        });
+
+        it('the sorted array should be in the correct order', function() {
+            expect(arrayToSort[0]).toBe(first);
+            expect(arrayToSort[1]).toBe(second);
+            expect(arrayToSort[2]).toBe(third);
+        });
+    });
+
+    describe('and the ComparatorBuilder is inverted', function() {
+        beforeEach(function() {
+            comparatorBuilder = comparatorBuilder.invert();
+        });
+
+        describe('and the ComparatorBuilder sorts an array (which requires both comparators)', function() {
+            var arrayToSort;
+
+            beforeEach(function() {
+                arrayToSort = [ third, first, second ];
+
+                arrayToSort.sort(comparatorBuilder.toComparator());
+            });
+
+            it('the first comparator should be invoked', function() {
+                expect(comparatorOne).toHaveBeenCalled();
+            });
+
+            it('the second comparator should be invoked', function() {
+                expect(comparatorTwo).toHaveBeenCalled();
+            });
+
+            it('the sorted array should be in the correct order', function() {
+                expect(arrayToSort[0]).toBe(third);
+                expect(arrayToSort[1]).toBe(second);
+                expect(arrayToSort[2]).toBe(first);
+            });
+        });
+    });
+});
