eef75b6087b0ab485e77306618160f6a26dd9d94
Bryan Ingle <bryaningle@gmail.com>
2016-01-31 14:28:53 -0600

    Reformat code. Fix indents.

--- lang/attributes.js
+++ lang/attributes.js
@@ -3,77 +3,77 @@ var _ = require('lodash');
 var assert = require('./assert');
 
 module.exports = function() {
-    'use strict';
+	'use strict';
 
-    var attributes = {
-        has: function(target, propertyNames) {
-            assert.argumentIsRequired(target, 'target', Object);
-            assert.argumentIsRequired(propertyNames, 'propertyNames', String);
+	var attributes = {
+		has: function(target, propertyNames) {
+			assert.argumentIsRequired(target, 'target', Object);
+			assert.argumentIsRequired(propertyNames, 'propertyNames', String);
 
-            var propertyNameArray = getPropertyNameArray(propertyNames);
-            var propertyTarget = getPropertyTarget(target, propertyNameArray, false);
+			var propertyNameArray = getPropertyNameArray(propertyNames);
+			var propertyTarget = getPropertyTarget(target, propertyNameArray, false);
 
-            return propertyTarget !== null && _.has(propertyTarget, _.last(propertyNameArray));
-        },
+			return propertyTarget !== null && _.has(propertyTarget, _.last(propertyNameArray));
+		},
 
-        read: function(target, propertyNames) {
-            assert.argumentIsRequired(target, 'target', Object);
-            assert.argumentIsRequired(propertyNames, 'propertyNames', String);
+		read: function(target, propertyNames) {
+			assert.argumentIsRequired(target, 'target', Object);
+			assert.argumentIsRequired(propertyNames, 'propertyNames', String);
 
-            var propertyNameArray = getPropertyNameArray(propertyNames);
-            var propertyTarget = getPropertyTarget(target, propertyNameArray, false);
+			var propertyNameArray = getPropertyNameArray(propertyNames);
+			var propertyTarget = getPropertyTarget(target, propertyNameArray, false);
 
-            var returnRef;
+			var returnRef;
 
-            if (propertyTarget) {
-                var propertyName = _.last(propertyNameArray);
+			if (propertyTarget) {
+				var propertyName = _.last(propertyNameArray);
 
-                returnRef = propertyTarget[propertyName];
-            } else {
-                returnRef = undefined;
-            }
+				returnRef = propertyTarget[propertyName];
+			} else {
+				returnRef = undefined;
+			}
 
-            return returnRef;
-        },
+			return returnRef;
+		},
 
-        write: function(target, propertyNames, value) {
-            assert.argumentIsRequired(target, 'target', Object);
-            assert.argumentIsRequired(propertyNames, 'propertyNames', String);
+		write: function(target, propertyNames, value) {
+			assert.argumentIsRequired(target, 'target', Object);
+			assert.argumentIsRequired(propertyNames, 'propertyNames', String);
 
-            var propertyNameArray = getPropertyNameArray(propertyNames);
-            var propertyTarget = getPropertyTarget(target, propertyNameArray, true);
+			var propertyNameArray = getPropertyNameArray(propertyNames);
+			var propertyTarget = getPropertyTarget(target, propertyNameArray, true);
 
-            var propertyName = _.last(propertyNameArray);
+			var propertyName = _.last(propertyNameArray);
 
-            propertyTarget[propertyName] = value;
-        }
-    };
+			propertyTarget[propertyName] = value;
+		}
+	};
 
-    function getPropertyNameArray(propertyNames) {
-        return propertyNames.split('.');
-    }
+	function getPropertyNameArray(propertyNames) {
+		return propertyNames.split('.');
+	}
 
-    function getPropertyTarget(target, propertyNameArray, create) {
-        var returnRef;
+	function getPropertyTarget(target, propertyNameArray, create) {
+		var returnRef;
 
-        var propertyTarget = target;
+		var propertyTarget = target;
 
-        for (var i = 0; i < (propertyNameArray.length - 1); i++) {
-            var propertyName = propertyNameArray[i];
+		for (var i = 0; i < (propertyNameArray.length - 1); i++) {
+			var propertyName = propertyNameArray[i];
 
-            if (_.has(propertyTarget, propertyName)) {
-                propertyTarget = propertyTarget[propertyName];
-            } else if (create) {
-                propertyTarget = propertyTarget[propertyName] = { };
-            } else {
-                propertyTarget = null;
+			if (_.has(propertyTarget, propertyName)) {
+				propertyTarget = propertyTarget[propertyName];
+			} else if (create) {
+				propertyTarget = propertyTarget[propertyName] = {};
+			} else {
+				propertyTarget = null;
 
-                break;
-            }
-        }
+				break;
+			}
+		}
 
-        return propertyTarget;
-    }
+		return propertyTarget;
+	}
 
-    return attributes;
+	return attributes;
 }();
