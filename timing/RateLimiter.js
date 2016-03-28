505b9240683d4f1793250761778bbe02a21ecfaf
Bryan Ingle <bryan.ingle@barchart.com>
2016-03-28 14:22:26 -0500

    Tweak rate limiter

--- timing/RateLimiter.js
+++ timing/RateLimiter.js
@@ -15,12 +15,11 @@ module.exports = function() {
 	var logger = log4js.getLogger('common/timing/RateLimiter');
 
 	var RateLimiter = Disposable.extend({
-		init: function(windowMaximumCount, windowDurationMilliseconds, concurrency) {
+		init: function(windowMaximumCount, windowDurationMilliseconds) {
 			this._super();
 
 			assert.argumentIsRequired(windowMaximumCount, 'windowMaximumCount', Number);
 			assert.argumentIsRequired(windowDurationMilliseconds, 'windowDurationMilliseconds', Number);
-			assert.argumentIsOptional(concurrency, 'concurrency', Number);
 
 			this._windowMaximumCount = windowMaximumCount;
 			this._windowDurationMilliseconds = windowDurationMilliseconds;
@@ -31,20 +30,6 @@ module.exports = function() {
 
 			this._windowStart = null;
 			this._windowCounter = 0;
-
-			var executor;
-
-			if (_.isNumber(concurrency) && concurrency > 0) {
-				executor = guard(guard.n(concurrency), function(actionToExecute) {
-					return actionToExecute();
-				});
-			} else {
-				executor = function(actionToExecute) {
-					return actionToExecute();
-				};
-			}
-
-			this._executor = executor;
 		},
 
 		enqueue: function(actionToEnqueue) {
@@ -60,10 +45,6 @@ module.exports = function() {
 
 			that._workQueue.enqueue(function() {
 				return when.try(function() {
-					checkWindow.call(that);
-
-					that._windowCounter = that._windowCounter + 1;
-
 					return actionToEnqueue();
 				}).then(function(result) {
 					deferred.resolve(result);
@@ -97,41 +78,32 @@ module.exports = function() {
 			return;
 		}
 
-		checkWindow.call(that);
+		if (that._windowStart === null) {
+			var timestamp = new Date();
 
-		if (that._windowCounter < this._windowMaximumCount) {
-			that._executor(that._workQueue.dequeue())
-				.then(function() {
-					checkStart.call(that);
-				});
-		}
-	}
-
-	var checkWindow = function() {
-		var that = this;
-
-		if (that.getIsDisposed()) {
-			return;
-		}
-
-		if (that._windowStart !== null) {
-			return;
-		}
-
-		var timestamp = new Date();
-
-		that._windowStart = timestamp.getTime();
-		that._windowCounter = 0;
-
-		var resetWindow = function() {
-			that._windowStart = null;
+			that._windowStart = timestamp.getTime();
 			that._windowCounter = 0;
 
-			checkStart.call(that);
-		};
+			var resetWindow = function() {
+				that._windowStart = null;
+				that._windowCounter = 0;
 
-		that._scheduler.schedule(resetWindow, that._windowDurationMilliseconds, 'Rate Limiter Window Reset');
-	};
+				checkStart.call(that);
+			};
+
+			that._scheduler.schedule(resetWindow, that._windowDurationMilliseconds, 'Rate Limiter Window Reset');
+		}
+
+		if (that._windowCounter < this._windowMaximumCount) {
+			that._windowCounter = that._windowCounter + 1;
+
+			var actionToExecute = that._workQueue.dequeue();
+
+			actionToExecute().then(function() {
+				checkStart.call(that);
+			});
+		}
+	}
 
 	return RateLimiter;
 }();
