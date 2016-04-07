40f3b3abc0d53ca09abd404068ea8433304646b3
Bryan Ingle <bryan.ingle@barchart.com>
2016-04-07 13:29:36 -0500

    Release. Bump version number

--- test/dist/barchart-common-tests-1.0.js
+++ test/dist/barchart-common-tests-1.0.js
@@ -508,7 +508,12 @@ module.exports = function() {
 	var attributes = {
 		has: function(target, propertyNames) {
 			assert.argumentIsRequired(target, 'target', Object);
-			assert.argumentIsRequired(propertyNames, 'propertyNames', String);
+
+			if (_.isArray(propertyNames)) {
+				assert.argumentIsArray(propertyNames, 'propertyNames', String);
+			} else {
+				assert.argumentIsRequired(propertyNames, 'propertyNames', String);
+			}
 
 			var propertyNameArray = getPropertyNameArray(propertyNames);
 			var propertyTarget = getPropertyTarget(target, propertyNameArray, false);
@@ -18911,6 +18916,30 @@ describe('When "attributes.has" is used to check a top-level property', function
 	});
 });
 
+describe('When "attributes.has" is used to check a top-level property (with an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	describe("and the property exists", function() {
+		it("should return true", function() {
+			expect(attributes.has(target, [ "test" ])).toEqual(true);
+		});
+	});
+
+	describe("and the property does not exist", function() {
+		it("should return true", function() {
+			expect(attributes.has(target, [ "name" ])).toEqual(false);
+		});
+	});
+});
+
 describe('When "attributes.has" is used to check a second-level property', function() {
 	'use strict';
 
@@ -18943,6 +18972,70 @@ describe('When "attributes.has" is used to check a second-level property', funct
 	});
 });
 
+describe('When "attributes.has" is used to check a second-level property (with an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			nested: {
+				test: 123
+			}
+		};
+	});
+
+	describe("and the property exists", function() {
+		it("should return true", function() {
+			expect(attributes.has(target, [ "nested", "test" ])).toEqual(true);
+		});
+	});
+
+	describe("and the property does not exist", function() {
+		it("should return true", function() {
+			expect(attributes.has(target, [ "nested", "name" ])).toEqual(false);
+		});
+	});
+
+	describe("and the top-level property does not exist", function() {
+		it("should return true", function() {
+			expect(attributes.has(target, [ "wrong", "name" ])).toEqual(false);
+		});
+	});
+});
+
+describe('When "attributes.has" is called with an empty string', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	it("should return false", function() {
+		expect(attributes.has(target, "")).toEqual(false);
+	});
+});
+
+describe('When "attributes.has" is called with a zero-length array', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	it("should return false", function() {
+		expect(attributes.has(target, [ ])).toEqual(false);
+	});
+});
+
 describe('When "attributes.read" is used to get a top-level property', function() {
 	'use strict';
 
@@ -18966,6 +19059,33 @@ describe('When "attributes.read" is used to get a top-level property', function(
 	describe("and the property does not exist", function() {
 		it("should be undefined", function() {
 			expect(attributes.read(target, "nested.name")).toBe(undefined);
+		});
+	});
+});
+
+describe('When "attributes.read" is used to get a top-level property (with an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			nested: {
+				test: 123
+			}
+		};
+	});
+
+
+	describe("and the property exists", function() {
+		it("should return the property value", function() {
+			expect(attributes.read(target, [ "nested", "test" ])).toEqual(123);
+		});
+	});
+
+	describe("and the property does not exist", function() {
+		it("should be undefined", function() {
+			expect(attributes.read(target, [ "nested", "name" ])).toBe(undefined);
 		});
 	});
 });
@@ -19002,6 +19122,70 @@ describe('When "attributes.read" is used to get a second-level property', functi
 	});
 });
 
+describe('When "attributes.read" is used to get a second-level property (with an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			nested: {
+				test: 123
+			}
+		};
+	});
+
+	describe("and the property exists", function() {
+		it("should return the property value", function() {
+			expect(attributes.read(target, [ "nested", "test" ])).toEqual(123);
+		});
+	});
+
+	describe("and the property does not exist", function() {
+		it("should be undefined", function() {
+			expect(attributes.read(target, [ "nested", "name" ])).toBe(undefined);
+		});
+	});
+
+	describe("and the top-level property does not exist", function() {
+		it("should be undefined", function() {
+			expect(attributes.read(target, [ "wrong", "name" ])).toBe(undefined);
+		});
+	});
+});
+
+describe('When "attributes.read" is called with an empty string', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	it("should return an undefined value", function() {
+		expect(attributes.read(target, "")).toBe(undefined);
+	});
+});
+
+describe('When "attributes.read" is called with a zero-length array', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	it("should return an undefined value", function() {
+		expect(attributes.read(target, [ ])).toBe(undefined);
+	});
+});
+
 describe('When "attributes.write" is used to set a top-level property', function() {
 	'use strict';
 
@@ -19026,6 +19210,38 @@ describe('When "attributes.write" is used to set a top-level property', function
 	describe("and the property does not exist", function() {
 		beforeEach(function() {
 			attributes.write(target, "name", "Alice");
+		});
+
+		it("the property value should be created and set", function() {
+			expect(target.name).toEqual("Alice");
+		});
+	});
+});
+
+describe('When "attributes.write" is used to set a top-level property (with an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	describe("and the property exists", function() {
+		beforeEach(function() {
+			attributes.write(target, [ "test" ], "four-five-six");
+		});
+
+		it("the property value should be overwritten", function() {
+			expect(target.test).toEqual("four-five-six");
+		});
+	});
+
+	describe("and the property does not exist", function() {
+		beforeEach(function() {
+			attributes.write(target, [ "name" ], "Alice");
 		});
 
 		it("the property value should be created and set", function() {
@@ -19078,6 +19294,50 @@ describe('When "attributes.write" is used to set a second-level property', funct
 	});
 });
 
+describe('When "attributes.write" is used to set a second-level property (using an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			nested: {
+				test: 123
+			}
+		};
+	});
+
+	describe("and the property exists", function() {
+		beforeEach(function() {
+			attributes.write(target, [ "nested", "test" ], "four-five-six");
+		});
+
+		it("the property value should be overwritten", function() {
+			expect(target.nested.test).toEqual("four-five-six");
+		});
+	});
+
+	describe("and the second-level property does not exist", function() {
+		beforeEach(function() {
+			attributes.write(target, [ "nested", "name" ], "Alice");
+		});
+
+		it("the property value should be created and set", function() {
+			expect(target.nested.name).toEqual("Alice");
+		});
+	});
+
+	describe("and the top-level property does not exist", function() {
+		beforeEach(function() {
+			attributes.write(target, [ "x", "y" ], "z");
+		});
+
+		it("the top-level and second properties value should be created and set", function() {
+			expect(target.x.y).toEqual("z");
+		});
+	});
+});
+
 describe('When "attributes.erase" is used to remove a top-level property', function() {
 	'use strict';
 
@@ -19102,6 +19362,38 @@ describe('When "attributes.erase" is used to remove a top-level property', funct
 	describe("and the property does not exist", function() {
 		beforeEach(function() {
 			attributes.erase(target, "name");
+		});
+
+		it("the target should be unaffected", function() {
+			expect(target.hasOwnProperty("test")).toEqual(true);
+		});
+	});
+});
+
+describe('When "attributes.erase" is used to remove a top-level property (using an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			test: 123
+		};
+	});
+
+	describe("and the property exists", function() {
+		beforeEach(function() {
+			attributes.erase(target, [ "test" ]);
+		});
+
+		it("the property value not exist", function() {
+			expect(target.hasOwnProperty("test")).toEqual(false);
+		});
+	});
+
+	describe("and the property does not exist", function() {
+		beforeEach(function() {
+			attributes.erase(target, [ "name" ]);
 		});
 
 		it("the target should be unaffected", function() {
@@ -19149,6 +19441,55 @@ describe('When "attributes.erase" is used to remove a second-level property', fu
 	describe("and the top-level property does not exist", function() {
 		beforeEach(function() {
 			attributes.erase(target, "x.y");
+		});
+
+		it("the target should be unaffected", function() {
+			expect(target.hasOwnProperty("nested")).toEqual(true);
+			expect(target.nested.hasOwnProperty("test")).toEqual(true);
+		});
+	});
+});
+
+
+describe('When "attributes.erase" is used to remove a second-level property (using an array)', function() {
+	'use strict';
+
+	var target;
+
+	beforeEach(function() {
+		target = {
+			nested: {
+				test: 123
+			}
+		};
+	});
+
+
+	describe("and the property exists", function() {
+		beforeEach(function() {
+			attributes.erase(target, [ "nested", "test" ]);
+		});
+
+		it("the property value not exist", function() {
+			expect(target.hasOwnProperty("nested")).toEqual(true);
+			expect(target.nested.hasOwnProperty("test")).toEqual(false);
+		});
+	});
+
+	describe("and the second-level property does not exist", function() {
+		beforeEach(function() {
+			attributes.erase(target, [ "nested", "name" ]);
+		});
+
+		it("the target should be unaffected", function() {
+			expect(target.hasOwnProperty("nested")).toEqual(true);
+			expect(target.nested.hasOwnProperty("test")).toEqual(true);
+		});
+	});
+
+	describe("and the top-level property does not exist", function() {
+		beforeEach(function() {
+			attributes.erase(target, [ "x", "y" ]);
 		});
 
 		it("the target should be unaffected", function() {
