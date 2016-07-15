2ed97fd262bf63777355436e47c092d7339caeb0
Bryan Ingle <bryaningle@gmail.com>
2016-07-15 07:19:05 -0500

    More functions for Tree collection (untested)

--- collections/Tree.js
+++ collections/Tree.js
@@ -1,11 +1,13 @@
 var Class = require('class.extend');
 
+var assert = require('./../lang/assert');
+
 module.exports = function() {
 	'use strict';
 
 	var Tree = Class.extend({
-		init: function(item, parent) {
-			this._item = item;
+		init: function(value, parent) {
+			this._value = value;
 
 			this._parent = parent || null;
 			this._children = [ ];
@@ -19,44 +21,91 @@ module.exports = function() {
 			return this._children;
 		},
 
-		add: function(item) {
-			this._children.push(new Tree(this, item));
+		getValue: function() {
+			return this._value;
 		},
 
-		remove: function(item) {
+		getIsLeaf: function() {
+			return this._children.length === 0;
+		},
+
+		getIsRoot: function() {
+			return this._parent === null;
+		},
+
+		addChild: function(value) {
+			var returnRef = new Tree(this, value);
+
+			this._children.push(returnRef);
+
+			return returnRef;
+		},
+
+		removeChild: function(node) {
+			var returnRef = null;
+
 			for (var i = this._children.length - 1; !(i < 0); i--) {
 				var child = this._children[i];
 
-				if (child.getItem() === item) {
+				if (child === node) {
 					this._children.splice(i, 1);
 
 					child._parent = null;
+					child._children = [ ];
+
+					break;
 				}
 			}
 		},
 
-		find: function(predicate, childrenFirst) {
+		findChild: function(predicate) {
 			var returnRef = null;
 
-			if (!chilrenFirst && predicate(this.getItem())) {
-				returnRef = this;
-			}
+			for (var i = 0; i < this._children.length; i++) {
+				var child = this._children[i];
 
-			if (returnRef === null) {
-				for (var i = 0; i < that._children.length; i++) {
-					returnRef = that._children[i].find(predicate, childrenFirst);
+				if (predicate(child.getValue(), child)) {
+					returnRef = child;
 
-					if (returnRef !== null) {
-						break;
-					}
-				}
-
-				if (childrenFirst && returnRef === null && predicate(this.getItem())) {
-					returnRef = this;
+					break;
 				}
 			}
 
 			return returnRef;
+		},
+
+		walk: function(walkAction, childrenFirst, includeCurrentNode) {
+			if (childrenFirst && includeCurrentNode) {
+				walkAction(this.getValue(), this);
+			}
+
+			for (var i = 0; i < this._children.length; i++) {
+				var child = this._children[i];
+
+				if (childrenFirst) {
+					this.walk(walkAction, true, false);
+				}
+
+				walkAction(child.getValue(), child);
+
+				if (!childrenFirst) {
+					this.walk(walkAction, false, false);
+				}
+			}
+
+			if (!childrenFirst && includeCurrentNode) {
+				walkAction(this.getValue(), this);
+			}
+		},
+
+		climb: function(climbAction, includeCurrentNode) {
+			if (includeCurrentNode)	{
+				climbAction(this.getValue(), this);
+			}
+
+			if (this._parent !== null) {
+				this._parent.climb(climbAction, true);
+			}
 		},
 
 		toString: function() {
