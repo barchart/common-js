d78a8a38bed9c465ade8793e875d75de84a1d0d4
Bryan Ingle <bryan.ingle@barchart.com>
2016-07-21 17:21:40 -0500

    Add date formatting utilities

---
+++ lang/date.js
@@ -0,0 +1,71 @@
+module.exports = (() => {
+	'use strict';
+
+	const utilities = {
+		getShortDay(date) {
+			const day = date.getDay();
+
+			return days[day].short;
+		},
+
+		getDate(date) {
+			return date.getDate();
+		},
+
+		getDateOrdinal(date) {
+			var d = utilities.getDate(date);
+			const remainder = d % 10;
+
+			let returnRef;
+
+			if (remainder === 1 && d !== 11) {
+				returnRef = 'st';
+			} else if (remainder === 2 && d !== 12) {
+				returnRef = 'nd';
+			} else if (remainder === 3) {
+				returnRef = 'rd';
+			} else {
+				returnRef = 'th';
+			}
+
+			return returnRef;
+		},
+
+		getShortMonth(date) {
+			const month = date.getMonth();
+
+			return months[month].short;
+		},
+
+		getYear(date) {
+			return date.getFullYear();
+		}
+	};
+
+	const days = [
+		{ short: 'Sun' },
+		{ short: 'Mon' },
+		{ short: 'Tue' },
+		{ short: 'Wed' },
+		{ short: 'Thu' },
+		{ short: 'Fri' },
+		{ short: 'Sat' }
+	];
+
+	const months = [
+		{ short: 'Jan' },
+		{ short: 'Feb' },
+		{ short: 'Mar' },
+		{ short: 'Apr' },
+		{ short: 'May' },
+		{ short: 'Jun' },
+		{ short: 'Jul' },
+		{ short: 'Aug' },
+		{ short: 'Sep' },
+		{ short: 'Oct' },
+		{ short: 'Nov' },
+		{ short: 'Dev' }
+	];
+
+	return utilities;
+})();
