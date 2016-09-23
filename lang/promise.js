25b50b0d6818d1e78743b51faba0a11f98ca06f5
Bryan Ingle <bryan.ingle@barchart.com>
2016-09-23 11:29:08 -0500

    Add throttler for promise-based mapping

--- lang/promise.js
+++ lang/promise.js
@@ -39,6 +39,56 @@ module.exports = (() => {
 					}
 				});
 			});
+		},
+
+		map(items, mapper, concurrency) {
+			assert.argumentIsArray(items, 'items');
+			assert.argumentIsRequired(mapper, 'mapper', Function);
+			assert.argumentIsOptional(concurrency, 'concurrency', Number);
+
+			if (items.length === 0) {
+				return Promise.resolve([ ]);
+			}
+
+			return new Promise((resolveCallback, rejectCallback) => {
+				const concurrencyToUse = Math.max(1, concurrency || 0);
+
+				let current = 0;
+
+				let active = 0;
+				let complete = 0;
+
+				let results = Array.of(items.length);
+
+				const start = (index) => {
+					current = index;
+					active = active + 1;
+
+					const item = items[current];
+
+					Promise.resolve()
+						.then(() => {
+							return mapper(item);
+						}).then((result) => {
+							results[index] = result;
+
+							active = active - 1;
+							complete = complete + 1;
+
+							if (complete === items.length) {
+								resolveCallback(results);
+							} else if (active < concurrencyToUse && complete < items.length) {
+								start(current + 1);
+							}
+						}).catch((error) => {
+							rejectCallback(error);
+						});
+				};
+
+				for (var i = 0; i < items.length && active < concurrencyToUse && complete < items.length; i++) {
+					start(i);
+				}
+			});
 		}
 	};
 })();
--- test/specs/lang/promiseSpec.js
+++ test/specs/lang/promiseSpec.js
@@ -164,3 +164,235 @@ describe('When a timeout is set for a promise', function() {
 		});
 	});
 });
+
+describe('When mapping an array through an promise-based function', function() {
+	'use strict';
+
+	describe('and the array has no items', function() {
+		var mapPromise;
+
+		var mapItems;
+		var mapSpy;
+
+		beforeEach(function() {
+			mapPromise = promise.map(mapItems = [ ], mapSpy = jasmine.createSpy('mapSpy'));
+		});
+
+		it('the result will be an array', function(done) {
+			mapPromise.then(function(results) {
+				expect(results instanceof Array).toEqual(true);
+
+				done();
+			});
+		});
+
+		it('the resulting array will be the same size as the input array', function(done) {
+			mapPromise.then(function(results) {
+				expect(results.length).toEqual(mapItems.length);
+
+				done();
+			});
+		});
+
+		it('the mapper function will be not have been called', function(done) {
+			mapPromise.then(function(results) {
+				expect(mapSpy.calls.count()).toEqual(0);
+
+				done();
+			});
+		});
+	});
+
+	describe('and the array has two items', function() {
+		var mapPromise;
+
+		var mapItems;
+		var mapSpy;
+
+		beforeEach(function() {
+			mapPromise = promise.map(mapItems = [ 'x', 'y' ], mapSpy = jasmine.createSpy('mapSpy'));
+		});
+
+		it('the result will be an array', function(done) {
+			mapPromise.then(function(results) {
+				expect(results instanceof Array).toEqual(true);
+
+				done();
+			});
+		});
+
+		it('the resulting array have two items', function(done) {
+			mapPromise.then(function(results) {
+				expect(results.length).toEqual(2);
+
+				done();
+			});
+		});
+
+		it('the mapper function to have been called twice', function(done) {
+			mapPromise.then(function(results) {
+				expect(mapSpy.calls.count()).toEqual(2);
+
+				done();
+			});
+		});
+
+		it('the mapper function will have been called once with the first item', function(done) {
+			mapPromise.then(function(results) {
+				expect(mapSpy).toHaveBeenCalledWith(mapItems[0]);
+
+				done();
+			});
+		});
+
+		it('the mapper function will have been called once with the second item', function(done) {
+			mapPromise.then(function(results) {
+				expect(mapSpy).toHaveBeenCalledWith(mapItems[1]);
+
+				done();
+			});
+		});
+	});
+
+	describe('and the array has three items', function() {
+		var mapPromise;
+
+		var mapItems;
+		var mapSpy;
+
+		beforeEach(function() {
+			mapItems = [ 'a', 'b', 'c' ];
+		});
+
+		describe('and the concurrency level is one', function() {
+			beforeEach(function() {
+				mapPromise = promise.map(mapItems, mapSpy = getMapSpy(), 1);
+			});
+
+			it('the maximum concurrency level should be one', function(done) {
+				mapPromise.then(function(results) {
+					expect(getMaximumConcurrency(results)).toEqual(1);
+
+					done();
+				});
+			});
+
+			it('the actual concurrency for the first item should be one', function(done) {
+				mapPromise.then(function(results) {
+					expect(getConcurrency(results, 0)).toEqual(1);
+
+					done();
+				});
+			});
+		});
+
+		describe('and the concurrency level is two', function() {
+			beforeEach(function() {
+				mapPromise = promise.map(mapItems, mapSpy = getMapSpy(), 2);
+			});
+
+			it('the maximum concurrency level should be two', function(done) {
+				mapPromise.then(function(results) {
+					expect(getMaximumConcurrency(results)).toEqual(2);
+
+					done();
+				});
+			});
+
+			it('the actual concurrency for the first item should be two', function(done) {
+				mapPromise.then(function(results) {
+					expect(getConcurrency(results, 0)).toEqual(2);
+
+					done();
+				});
+			});
+		});
+
+		describe('and the concurrency level is three', function() {
+			beforeEach(function() {
+				mapPromise = promise.map(mapItems, mapSpy = getMapSpy(), 3);
+			});
+
+			it('the maximum concurrency level should be three', function(done) {
+				mapPromise.then(function(results) {
+					expect(getMaximumConcurrency(results)).toEqual(3);
+
+					done();
+				});
+			});
+
+			it('the actual concurrency for the first item should be three', function(done) {
+				mapPromise.then(function(results) {
+					expect(getConcurrency(results, 0)).toEqual(3);
+
+					done();
+				});
+			});
+		});
+
+		describe('and the concurrency level is four', function() {
+			beforeEach(function() {
+				mapPromise = promise.map(mapItems, mapSpy = getMapSpy(), 4);
+			});
+
+			it('the maximum concurrency level should be three', function(done) {
+				mapPromise.then(function(results) {
+					expect(getMaximumConcurrency(results)).toEqual(3);
+
+					done();
+				});
+			});
+
+			it('the actual concurrency for the first item should be three', function(done) {
+				mapPromise.then(function(results) {
+					expect(getConcurrency(results, 0)).toEqual(3);
+
+					done();
+				});
+			});
+		});
+	});
+
+	var getMapSpy = function() {
+		return jasmine.createSpy('mapSpy').and.callFake(function() {
+			var startDate = new Date();
+
+			return new Promise(function(resolveCallback, rejectCallback) {
+				setTimeout(function() {
+					var endDate = new Date();
+
+					resolveCallback({
+						start: startDate.getTime(),
+						end: endDate.getTime()
+					});
+				}, 10);
+			});
+		});
+	};
+
+	var getConcurrency = function(results, index) {
+		var current = results[index];
+
+		var concurrency = 0;
+
+		for (var i = 0; i < results.length; i++) {
+			var other = results[i];
+
+			if (!(other.end <= current.start || other.start >= current.end)) {
+				concurrency = concurrency + 1;
+			}
+		}
+
+		return concurrency;
+	};
+
+	var getMaximumConcurrency = function(results) {
+		var maximum = 0;
+
+		for (var i = 0; i < results.length; i++) {
+			maximum = Math.max(getConcurrency(results, i), maximum);
+		}
+
+		return maximum;
+	};
+});
