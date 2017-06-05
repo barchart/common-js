1962665cd22d8918268adb6fc93d0e3e54c0359e
Bryan Ingle <bryan.ingle@barchart.com>
2017-06-05 11:13:05 -0500

    Add unit tests for exponential backoff utility

--- test/specs/timing/SchedulerSpec.js
+++ test/specs/timing/SchedulerSpec.js
@@ -72,3 +72,165 @@ describe('When a Scheduler is constructed', function() {
 		});
 	});
 });
+
+describe('When a backoff is used', function() {
+	'use strict';
+
+	var scheduler;
+
+	beforeEach(function() {
+		scheduler = new Scheduler();
+	});
+
+	describe('that succeeds immediately', function() {
+		var spyAction;
+		var spyFailure;
+
+		var actualResult;
+		var successfulResult;
+
+		beforeEach(function(done) {
+			spyAction = jasmine.createSpy('spyAction').and.callFake(function () {
+				return successfulResult = 'ok computer';
+			});
+
+			spyFailure = jasmine.createSpy('spyFailure');
+
+			scheduler.backoff(spyAction, 5, 'succeeds immediately', 1, spyFailure)
+				.then(function(r) {
+					actualResult = r;
+
+					done();
+				});
+		});
+
+		it('should call the "backoff" action one time', function() {
+			expect(spyAction.calls.count()).toEqual(1);
+		});
+
+		it('the promise result should match the expected result', function() {
+			expect(actualResult).toEqual(successfulResult);
+		});
+
+		it('should never call the "failure" action', function() {
+			expect(spyFailure.calls.count()).toEqual(0);
+		});
+	});
+
+	describe('that fails once before succeeding (by throwing error)', function() {
+		var spyAction;
+		var spyFailure;
+
+		var actualResult;
+		var successfulResult;
+
+		var x;
+
+		beforeEach(function(done) {
+			x = 0;
+
+			spyAction = jasmine.createSpy('spyAction').and.callFake(function () {
+				if (++x > 1) {
+					return successfulResult = 'ok computer';
+				} else {
+					throw new Error('nope...');
+				}
+			});
+
+			spyFailure = jasmine.createSpy('spyFailure');
+
+			scheduler.backoff(spyAction, 5, 'succeeds immediately', 5, spyFailure)
+				.then(function(r) {
+					actualResult = r;
+
+					done();
+				});
+		});
+
+		it('should call the "backoff" action two times', function() {
+			expect(spyAction.calls.count()).toEqual(2);
+		});
+
+		it('the promise result should match the expected result', function() {
+			expect(actualResult).toEqual(successfulResult);
+		});
+
+		it('the "failure" action should be called once', function() {
+			expect(spyFailure.calls.count()).toEqual(1);
+		});
+	});
+
+	describe('that fails twice before succeeding (by returning a specific "failure" value)', function() {
+		var spyAction;
+		var spyFailure;
+
+		var actualResult;
+		var successfulResult;
+
+		var x;
+
+		beforeEach(function(done) {
+			x = 0;
+
+			spyAction = jasmine.createSpy('spyAction').and.callFake(function () {
+				if (++x > 2) {
+					return successfulResult = [ 'ok computer' ];
+				} else {
+					return [ ];
+				}
+			});
+
+			spyFailure = jasmine.createSpy('spyFailure');
+
+			scheduler.backoff(spyAction, 5, 'succeeds immediately', 5, spyFailure, [ ])
+				.then(function(r) {
+					actualResult = r;
+
+					done();
+				});
+		});
+
+		it('should call the "backoff" action three times', function() {
+			expect(spyAction.calls.count()).toEqual(3);
+		});
+
+		it('the promise result should match the expected result', function() {
+			expect(actualResult).toEqual(successfulResult);
+		});
+
+		it('the "failure" action should be called twice', function() {
+			expect(spyFailure.calls.count()).toEqual(2);
+		});
+	});
+
+	describe('final failure is declared after four attempts', function() {
+		var spyAction;
+		var spyFailure;
+
+		var actualResult;
+		var successfulResult;
+
+		beforeEach(function(done) {
+			spyAction = jasmine.createSpy('spyAction').and.callFake(function () {
+				throw new Error('not gonna happen');
+			});
+
+			spyFailure = jasmine.createSpy('spyFailure');
+
+			scheduler.backoff(spyAction, 5, 'succeeds immediately', 3, spyFailure, [ ])
+				.catch(function(r) {
+					actualResult = r;
+
+					done();
+				});
+		});
+
+		it('should call the "backoff" action four times', function() {
+			expect(spyAction.calls.count()).toEqual(4);
+		});
+
+		it('the "failure" action should be called four times', function() {
+			expect(spyFailure.calls.count()).toEqual(4);
+		});
+	});
+});
--- timing/Scheduler.js
+++ timing/Scheduler.js
@@ -1,175 +1,177 @@
 const assert = require('./../lang/assert'),
-    Disposable = require('./../lang/Disposable'),
+	Disposable = require('./../lang/Disposable'),
 	is = require('./../lang/is'),
-    object = require('./../lang/object'),
+	object = require('./../lang/object'),
 	promise = require('./../lang/promise');
 
 module.exports = (() => {
-    'use strict';
+	'use strict';
 
-    class Scheduler extends Disposable {
-        constructor() {
-            super();
+	class Scheduler extends Disposable {
+		constructor() {
+			super();
 
-            this._timeoutBindings = {};
-            this._intervalBindings = {};
-        }
+			this._timeoutBindings = {};
+			this._intervalBindings = {};
+		}
 
-        schedule(actionToSchedule, millisecondDelay, actionDescription) {
-            assert.argumentIsRequired(actionToSchedule, 'actionToSchedule', Function);
-            assert.argumentIsRequired(millisecondDelay, 'millisecondDelay', Number);
-            assert.argumentIsOptional(actionDescription, 'actionDescription', String);
+		schedule(actionToSchedule, millisecondDelay, actionDescription) {
+			assert.argumentIsRequired(actionToSchedule, 'actionToSchedule', Function);
+			assert.argumentIsRequired(millisecondDelay, 'millisecondDelay', Number);
+			assert.argumentIsOptional(actionDescription, 'actionDescription', String);
 
-            if (this.getIsDisposed()) {
-                throw new Error('The Scheduler has been disposed.');
-            }
+			if (this.getIsDisposed()) {
+				throw new Error('The Scheduler has been disposed.');
+			}
 
-            let token;
+			let token;
 
-            const schedulePromise = promise.build((resolveCallback, rejectCallback) => {
-                const wrappedAction = () => {
-                    delete this._timeoutBindings[token];
+			const schedulePromise = promise.build((resolveCallback, rejectCallback) => {
+				const wrappedAction = () => {
+					delete this._timeoutBindings[token];
 
-                    try {
-                        resolveCallback(actionToSchedule());
-                    } catch (e) {
-                        rejectCallback(e);
-                    }
-                };
+					try {
+						resolveCallback(actionToSchedule());
+					} catch (e) {
+						rejectCallback(e);
+					}
+				};
 
-                token = setTimeout(wrappedAction, millisecondDelay);
-            });
+				token = setTimeout(wrappedAction, millisecondDelay);
+			});
 
-            this._timeoutBindings[token] = Disposable.fromAction(() => {
-                clearTimeout(token);
+			this._timeoutBindings[token] = Disposable.fromAction(() => {
+				clearTimeout(token);
 
-                delete this._timeoutBindings[token];
-            });
+				delete this._timeoutBindings[token];
+			});
 
-            return schedulePromise;
-        }
+			return schedulePromise;
+		}
 
-        repeat(actionToRepeat, millisecondInterval, actionDescription) {
-            assert.argumentIsRequired(actionToRepeat, 'actionToRepeat', Function);
-            assert.argumentIsRequired(millisecondInterval, 'millisecondInterval', Number);
-            assert.argumentIsOptional(actionDescription, 'actionDescription', String);
+		repeat(actionToRepeat, millisecondInterval, actionDescription) {
+			assert.argumentIsRequired(actionToRepeat, 'actionToRepeat', Function);
+			assert.argumentIsRequired(millisecondInterval, 'millisecondInterval', Number);
+			assert.argumentIsOptional(actionDescription, 'actionDescription', String);
 
-            if (this.getIsDisposed()) {
-                throw new Error('The Scheduler has been disposed.');
-            }
+			if (this.getIsDisposed()) {
+				throw new Error('The Scheduler has been disposed.');
+			}
 
-            const wrappedAction = () => {
-                try {
-                    actionToRepeat();
-                } catch (e) {
+			const wrappedAction = () => {
+				try {
+					actionToRepeat();
+				} catch (e) {
 
-                }
-            };
+				}
+			};
 
-            const token = setInterval(wrappedAction, millisecondInterval);
+			const token = setInterval(wrappedAction, millisecondInterval);
 
-            this._intervalBindings[token] = Disposable.fromAction(() => {
-                clearInterval(token);
+			this._intervalBindings[token] = Disposable.fromAction(() => {
+				clearInterval(token);
 
-                delete this._intervalBindings[token];
-            });
+				delete this._intervalBindings[token];
+			});
 
-            return this._intervalBindings[token];
-        }
+			return this._intervalBindings[token];
+		}
 
 		/**
-         * Attempts an action, repeating if necessary, using an exponential backoff.
+		 * Attempts an action, repeating if necessary, using an exponential backoff.
 		 *
 		 * @param {Function} actionToBackoff - The action to attempt. If it fails -- because an error is thrown, a promise is rejected, or the function returns a falsey value -- the action will be invoked again.
 		 * @param {number=} millisecondDelay - The amount of time to wait after the first failure. Subsequent failures are multiply this value by 2 ^ [number of failures]. So, a 1000 millisecond backoff would schedule attempts using the following delays: 0, 1000, 2000, 4000, 8000, etc.
 		 * @param {string=} actionDescription - Description of the action to attempt, used for logging purposes.
 		 * @param {number=} maximumAttempts - The number of attempts to before giving up.
-         * @param {Function=} maximumAttempts - If provided, will be invoked if a function is considered to be failing.
-         * @param {Object=} failureValue - If provided, will consider the result to have failed, if this value is returned (a deep equality check is used). If not provided, a "falsey" value will trigger a retry.
+		 * @param {Function=} maximumAttempts - If provided, will be invoked if a function is considered to be failing.
+		 * @param {Object=} failureValue - If provided, will consider the result to have failed, if this value is returned (a deep equality check is used). If not provided, a "falsey" value will trigger a retry.
 		 */
-		backoff(actionToBackoff, millisecondDelay, actionDescription, maximumAttempts, failureCallback, failureValue, falseyFailure) {
-            assert.argumentIsRequired(actionToBackoff, 'actionToBackoff', Function);
-            assert.argumentIsOptional(millisecondDelay, 'millisecondDelay', Number);
-            assert.argumentIsOptional(actionDescription, 'actionDescription', String);
-            assert.argumentIsOptional(maximumAttempts, 'maximumAttempts', Number);
+		backoff(actionToBackoff, millisecondDelay, actionDescription, maximumAttempts, failureCallback, failureValue) {
+			assert.argumentIsRequired(actionToBackoff, 'actionToBackoff', Function);
+			assert.argumentIsOptional(millisecondDelay, 'millisecondDelay', Number);
+			assert.argumentIsOptional(actionDescription, 'actionDescription', String);
+			assert.argumentIsOptional(maximumAttempts, 'maximumAttempts', Number);
 			assert.argumentIsOptional(failureCallback, 'failureCallback', Function);
 
-            if (this.getIsDisposed()) {
-                throw new Error('The Scheduler has been disposed.');
-            }
+			if (this.getIsDisposed()) {
+				throw new Error('The Scheduler has been disposed.');
+			}
 
-            const scheduleBackoff = (failureCount) => {
-            	if (failureCount > 0 && is.fn(failureCallback)) {
-            		failureCallback(failureCount);
+			const scheduleBackoff = (failureCount) => {
+				if (failureCount > 0 && is.fn(failureCallback)) {
+					failureCallback(failureCount);
 				}
 
-                if (maximumAttempts > 0 && failureCount > maximumAttempts) {
-                    return Promise.reject(`Maximum failures reached for ${actionDescription}`);
-                }
+				if (maximumAttempts > 0 && failureCount > maximumAttempts) {
+					return Promise.reject(`Maximum failures reached for ${actionDescription}`);
+				}
 
-                let backoffDelay;
+				let backoffDelay;
 
-                if (failureCount === 0) {
-                    backoffDelay = millisecondDelay;
-                } else {
-                    backoffDelay = (millisecondDelay || 1000) * Math.pow(2, failureCount);
-                }
+				if (failureCount === 0) {
+					backoffDelay = millisecondDelay;
+				} else {
+					backoffDelay = (millisecondDelay || 1000) * Math.pow(2, failureCount);
+				}
 
-                let failurePredicate;
+				let successPredicate;
 
-                if (is.undefined(failureValue)) {
-                    failurePredicate = (value) => !value;
-                } else {
-                    failurePredicate = (value) => (is.boolean(falseyFailure) && falseyFailure && !value) || !object.equals(value, failureValue);
-                }
+				if (is.undefined(failureValue)) {
+					successPredicate = (value) => value;
+				} else {
+					successPredicate = (value) => {
+						return !object.equals(value, failureValue);
+					}
+				}
 
-                return this.schedule(actionToBackoff, backoffDelay, (actionDescription || 'unspecified') + ', attempt ' + (failureCount + 1))
-                    .then((result) => {
-                        if (!failurePredicate(result)) {
-                            return result;
-                        } else {
-                            return scheduleBackoff(++failureCount);
-                        }
-                    }).catch((e) => {
-                        return scheduleBackoff(++failureCount);
-                    });
-            };
+				return this.schedule(actionToBackoff, backoffDelay, (actionDescription || 'unspecified') + ', attempt ' + (failureCount + 1))
+					.then((result) => {
+						if (successPredicate(result)) {
+							return result;
+						} else {
+							return scheduleBackoff(++failureCount);
+						}
+					}).catch((e) => {
+						return scheduleBackoff(++failureCount);
+					});
+			};
 
-            return scheduleBackoff(0);
-        }
+			return scheduleBackoff(0);
+		}
 
-        _onDispose() {
-            object.keys(this._timeoutBindings).forEach((key) => {
-                this._timeoutBindings[key].dispose();
-            });
+		_onDispose() {
+			object.keys(this._timeoutBindings).forEach((key) => {
+				this._timeoutBindings[key].dispose();
+			});
 
-            object.keys(this._intervalBindings).forEach((key) => {
-                this._intervalBindings[key].dispose();
-            });
+			object.keys(this._intervalBindings).forEach((key) => {
+				this._intervalBindings[key].dispose();
+			});
 
-            this._timeoutBindings = null;
-            this._intervalBindings = null;
-        }
+			this._timeoutBindings = null;
+			this._intervalBindings = null;
+		}
 
-        static schedule(actionToSchedule, millisecondDelay, actionDescription) {
-            const scheduler = new Scheduler();
+		static schedule(actionToSchedule, millisecondDelay, actionDescription) {
+			const scheduler = new Scheduler();
 
-            scheduler.schedule(actionToSchedule, millisecondDelay, actionDescription)
-                .then((result) => {
-                    scheduler.dispose();
+			scheduler.schedule(actionToSchedule, millisecondDelay, actionDescription)
+				.then((result) => {
+					scheduler.dispose();
 
-                    return result;
-                }).catch((e) => {
-                    scheduler.dispose();
+					return result;
+				}).catch((e) => {
+				scheduler.dispose();
 
-                    throw e;
-                });
-        }
+				throw e;
+			});
+		}
 
-        toString() {
-            return '[Scheduler]';
-        }
-    }
+		toString() {
+			return '[Scheduler]';
+		}
+	}
 
-    return Scheduler;
+	return Scheduler;
 })();
