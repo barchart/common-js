2c9f1dcd56d9fedcd26efed3337560e0e6c06821
Bryan Ingle <bryan.ingle@barchart.com>
2017-06-28 13:45:40 -0500

    Fix lint

---
+++ lang/memoize.js
@@ -0,0 +1,32 @@
+const is = require('./is');
+
+module.exports = (() => {
+	'use strict';
+
+	/**
+	 * Utilities for caching results of function invocations (a.k.a. memoization).
+	 *
+	 * @public
+	 * @module lang/memoize
+	 */
+	return {
+		/**
+		 * Memoizes a function that accepts a single argument only. Furthermore,
+		 * the parameter's toString function must return a unique value.
+		 *
+		 * @public
+		 * @param {Function} fn - The function to memoize. This function should accept one parameters whose "toString" function outputs a unique value.
+		 */
+		simple(fn) {
+			const cache = { };
+
+			return (x) => {
+				if (cache.hasOwnProperty(x)) {
+					return cache[x];
+				} else {
+					return cache[x] = fn(x);
+				}
+			};
+		}
+	};
+})();
