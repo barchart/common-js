33cecfe83d9a1854cbd82f40244197a8f54b1de3
Bryan Ingle <bryan.ingle@barchart.com>
2017-07-10 13:00:32 -0500

    Extend "not" specification tests

--- test/specs/specifications/NotSpecificationSpec.js
+++ test/specs/specifications/NotSpecificationSpec.js
@@ -72,3 +72,91 @@ describe('When a NotSpecification is constructed', function() {
 		});
 	});
 });
+
+describe('When a Specification (that always fails) is constructed', function() {
+	'use strict';
+
+	class DelegateSpecification extends Specification {
+		constructor(fn) {
+			super();
+
+			this._fn = fn;
+		}
+
+		_evaluate(data) {
+			return this._fn(data);
+		}
+	}
+
+	describe('and inverted', function () {
+		var specification;
+		var spy;
+
+		var result;
+
+		beforeEach(function () {
+			specification = new DelegateSpecification(
+				spy = jasmine.createSpy('fn').and.callFake(function (data) {
+					return false;
+				})
+			);
+
+			specification = specification.not();
+
+			result = specification.evaluate('abc');
+		});
+
+
+		it('should call the original specification', function () {
+			expect(spy).toHaveBeenCalled();
+		});
+
+		it('should pass', function () {
+			expect(result).toEqual(true);
+		});
+	});
+});
+
+describe('When a Specification (that always succeeds) is constructed', function() {
+	'use strict';
+
+	class DelegateSpecification extends Specification {
+		constructor(fn) {
+			super();
+
+			this._fn = fn;
+		}
+
+		_evaluate(data) {
+			return this._fn(data);
+		}
+	}
+
+	describe('and inverted', function () {
+		var specification;
+		var spy;
+
+		var result;
+
+		beforeEach(function () {
+			specification = new DelegateSpecification(
+				spy = jasmine.createSpy('fn').and.callFake(function (data) {
+					return true;
+				})
+			);
+
+			specification = specification.not();
+
+			result = specification.evaluate('abc');
+		});
+
+
+		it('should call the original specification', function () {
+			expect(spy).toHaveBeenCalled();
+		});
+
+		it('should pass', function () {
+			expect(result).toEqual(false);
+		});
+	});
+});
