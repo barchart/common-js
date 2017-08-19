cc3d3e26e28b9570c249b20c13265d80f26c3c32
Bryan Ingle <bryaningle@gmail.com>
2017-08-19 13:59:29 -0500

    JSDoc

--- commands/CommandHandler.js
+++ commands/CommandHandler.js
@@ -3,14 +3,30 @@ const assert = require('./../lang/assert');
 module.exports = (() => {
 	'use strict';
 
+	/**
+	 * An object that can perform an action.
+	 *
+	 * @public
+	 */
 	class CommandHandler {
 		constructor() {
 		}
 
+		/**
+		 * Execute the action.
+		 *
+		 * @param {*} data
+		 * @returns {*}
+		 */
 		process(data) {
 			return this._process(data);
 		}
 
+		/**
+		 * @protected
+		 * @param {*} data
+		 * @returns {*}
+		 */
 		_process(data) {
 			return true;
 		}
@@ -19,6 +35,13 @@ module.exports = (() => {
 			return '[CommandHandler]';
 		}
 
+		/**
+		 * Returns a function which executes the command.
+		 *
+		 * @public
+		 * @param {CommandHandler} commandHandler
+		 * @returns {function(*=)}
+		 */
 		static toFunction(commandHandler) {
 			assert.argumentIsRequired(commandHandler, 'commandHandler', CommandHandler, 'CommandHandler');
 
@@ -27,6 +50,14 @@ module.exports = (() => {
 			};
 		}
 
+		/**
+		 * Returns a {@link CommandHandler} that where execution is delegated
+		 * to a function.
+		 *
+		 * @public
+		 * @param {Function} handler - The function which the command delegates to.
+		 * @returns {CommandHandler}
+		 */
 		static fromFunction(handler) {
 			assert.argumentIsRequired(handler, 'handler', Function);
 
