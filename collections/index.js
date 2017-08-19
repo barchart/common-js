5b6b6866c18a3a0a056a8e4564ac3e0232dab688
Bryan Ingle <bryaningle@gmail.com>
2017-08-19 06:54:42 -0500

    Fix "index" scripts

--- collections/index.js
+++ collections/index.js
@@ -2,8 +2,10 @@ const Queue = require('./Queue'),
 	Stack = require('./Stack'),
 	Tree = require('./Tree'),
 	DisposableStack = require('./specialized/DisposableStack'),
+	CompoundMap = require('./specialized/CompoundMap'),
 	EvictingList = require('./specialized/EvictingList'),
-	EvictingMap = require('./specialized/EvictingMap');
+	EvictingMap = require('./specialized/EvictingMap'),
+	PriorityQueue = require('./specialized/PriorityQueue');
 
 const ComparatorBuilder = require('./sorting/ComparatorBuilder'),
 	comparators = require('./sorting/comparators');
@@ -18,9 +20,11 @@ module.exports = (() => {
 			comparators: comparators
 		},
 		Specialized: {
+			CompoundMap: CompoundMap,
 			DisposableStack: DisposableStack,
 			EvictingList: EvictingList,
-			EvictingMap: EvictingMap
+			EvictingMap: EvictingMap,
+			PriorityQueue: PriorityQueue
 		},
 		Stack: Stack,
 		Tree: Tree
--- lang/index.js
+++ lang/index.js
@@ -1,37 +1,53 @@
-const assert = require('./assert'),
-	array = require('./array'),
+const array = require('./array'),
+	assert = require('./assert'),
 	attributes = require('./attributes'),
 	connection = require('./connection'),
 	converters = require('./converters'),
 	date = require('./date'),
+	Day = require('./Day'),
+	Decimal = require('./Decimal'),
 	Disposable = require('./Disposable'),
+	Enum = require('./Enum'),
 	is = require('./is'),
 	mask = require('./mask'),
 	math = require('./math'),
+	memoize = require('./memoize'),
+	Money = require('./Money'),
 	object = require('./object'),
 	promise = require('./promise'),
 	random = require('./random'),
+	Rate = require('./Rate'),
 	string = require('./string'),
-	timezone = require('./timezone');
+	Timestamp = require('./Timestamp'),
+	timezone = require('./timezone'),
+	Timezones = require('./Timezones');
 
 module.exports = (() => {
 	'use strict';
 
 	return {
-		assert: assert,
 		array: array,
+		assert: assert,
 		attributes: attributes,
 		connection: connection,
 		converters: converters,
 		date: date,
+		Day: Day,
 		Disposable: Disposable,
+		Decimal: Decimal,
+		Enum: Enum,
 		is: is,
 		mask: mask,
 		math: math,
+		memoize: memoize,
+		Money: Money,
 		object: object,
 		promise: promise,
 		random: random,
+		Rate: Rate,
 		string: string,
-		timezone: timezone
+		Timestamp: Timestamp,
+		timezone: timezone,
+		Timezones: Timezones
 	};
 })();
