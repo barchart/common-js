33696db436f70073d1496cbd148fea0a954fa4a6
Bryan Ingle <bryaningle@gmail.com>
2017-08-29 07:35:24 -0500

    Fix Rate constructor, add JSDoc

--- lang/Rate.js
+++ lang/Rate.js
@@ -2,15 +2,26 @@ const assert = require('./assert'),
 	is = require('./is'),
 	memoize = require('./memoize');
 
-const Decimal = require('./Decimal');
+const Currency = require('./Currency'),
+	Decimal = require('./Decimal');
 
 module.exports = (() => {
 	'use strict';
 
+	/**
+	 * A component that represents an exchange rate, composed of a {@link Decimal}
+	 * value and two currencies -- a quote (i.e. the numerator) currency and a
+	 * base (i.e. denominator) currency.
+	 *
+	 * @public
+	 * @param {Number|String|Decimal} value - The rate
+	 * @param {Currency} numerator - The quote currency
+	 * @param {Currency} denominator - The base currency
+	 */
 	class Rate {
-		constructor(value, denominator, numerator) {
-			assert.argumentIsRequired(numerator, 'numerator', String);
-			assert.argumentIsRequired(denominator, 'denominator', String);
+		constructor(value, numerator, denominator) {
+			assert.argumentIsRequired(numerator, 'numerator', Currency, 'Currency');
+			assert.argumentIsRequired(denominator, 'denominator', Currency, 'Currency');
 
 			if (numerator === denominator) {
 				throw new Error('A rate cannot use two identical currencies.');
@@ -21,32 +32,81 @@ module.exports = (() => {
 			this._denominator = denominator;
 		}
 
+		/**
+		 * The rate.
+		 *
+		 * @public
+		 * @returns {Decimal}
+		 */
 		get decimal() {
 			return this._decimal;
 		}
 
+		/**
+		 * The numerator (i.e. quote) currency. In other words,
+		 * this is EUR of the EURUSD pair.
+		 *
+		 * @public
+		 * @returns {Currency}
+		 */
 		get numerator() {
 			return this._numerator;
 		}
 
+		/**
+		 * The quote (i.e. numerator) currency. In other words,
+		 * this is EUR of the EURUSD pair.
+		 *
+		 * @public
+		 * @returns {Currency}
+		 */
 		get quote() {
 			return this._numerator;
 		}
 
+		/**
+		 * The denominator (i.e. base) currency. In other words,
+		 * this is USD of the EURUSD pair.
+		 *
+		 * @public
+		 * @returns {Currency}
+		 */
 		get denominator() {
 			return this._denominator;
 		}
 
+		/**
+		 * The base (i.e. denominator) currency. In other words,
+		 * this is USD of the EURUSD pair.
+		 *
+		 * @public
+		 * @returns {Currency}
+		 */
 		get base() {
 			return this._denominator;
 		}
 
+		/**
+		 * Formats the currency pair as a string (e.g. "EURUSD" or "^EURUSD").
+		 *
+		 * @public
+		 * @param {Boolean=} useCarat - If true, a carat is used as a prefix to the resulting string.
+		 * @returns {string}
+		 */
 		formatPair(useCarat) {
 			assert.argumentIsOptional(useCarat, 'useCarat', Boolean);
 
 			return `${(useCarat ? '^' : '')}${this._numerator}${this._denominator}`;
 		}
 
+		/**
+		 * Creates a {@link Rate} instance, when given a value
+		 *
+		 * @public
+		 * @param {Number|String|Decimal} value - The rate.
+		 * @param {Currency} pair - A string that can be parsed as a currency pair.
+		 * @returns {Rate}
+		 */
 		static fromPair(value, pair) {
 			assert.argumentIsRequired(pair, 'pair', String);
 
