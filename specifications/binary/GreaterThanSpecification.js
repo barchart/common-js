f3a8e79e6cd2f1b5748438a219ca1de2363e3d09
Bryan Ingle <bryan.ingle@barchart.com>
2019-03-25 16:57:07 -0500

    Added new specifications

---
+++ specifications/binary/GreaterThanSpecification.js
@@ -0,0 +1,25 @@
+const assert = require('./../lang/assert');
+
+const Specification = require('./../Specification');
+
+module.exports = (() => {
+	'use strict';
+
+	class GreaterThanSpecification extends Specification {
+		constructor() {
+			super();
+		}
+
+		_evaluate(data) {
+			assert.argumentIsArray(data, 'data', Number);
+
+			return data.length === 2 && data[0] > data[1];
+		}
+
+		toString() {
+			return '[GreaterThanSpecification]';
+		}
+	}
+
+	return GreaterThanSpecification;
+})();
