4fe0a0a1dacb4e2d1959ee354a3a9caf91c45284
Bryan Ingle <bryan.ingle@barchart.com>
2019-10-09 15:52:38 -0500

    JSDoc

--- lang/Timezones.js
+++ lang/Timezones.js
@@ -29,7 +29,7 @@ module.exports = (() => {
 		 *
 		 * @public
 		 * @param {Number=} timestamp
-		 * @returns {*}
+		 * @returns {Number}
 		 */
 		getUtcOffset(timestamp) {
 			let timestampToUse;
