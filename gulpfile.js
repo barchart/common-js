abcd45eaed3fe1c196fe70c30b153ca6b4d1bce9
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-04-12 11:09:44 -0500

    Add buildspec.yml for AWS CodeBuild, update reports for JSHint

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,8 @@ gulp.task('release', gulp.series(
 gulp.task('lint', () => {
     return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./docs/**', '!./test/SpecRunner.js' ])
         .pipe(jshint({'esversion': 6}))
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', gulp.series('execute-tests'));
