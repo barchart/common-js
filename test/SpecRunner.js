8465707936eeb71231ee22e0d4b86b6dd4df862e
Bryan Ingle <bryaningle@gmail.com>
2022-02-11 10:33:07 -0700

    Release. Bump version number

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -5185,8 +5185,8 @@ module.exports = (() => {
      *
      * @static
      * @param {*} variable - The value to check.
-     * @param {String} variableName - The name of the value (used for formatting an error message).
-     * @param {*} type - The expected type of the argument.
+     * @param {String=} variableName - The name of the value (used for formatting an error message).
+     * @param {*=} type - The expected type of the argument.
      * @param {String=} typeDescription - The description of the expected type (used for formatting an error message).
      */
     argumentIsRequired(variable, variableName, type, typeDescription) {
@@ -5199,8 +5199,8 @@ module.exports = (() => {
      *
      * @static
      * @param {*} variable - The value to check.
-     * @param {String} variableName - The name of the value (used for formatting an error message).
-     * @param {*} type - The expected type of the argument.
+     * @param {String=} variableName - The name of the value (used for formatting an error message).
+     * @param {*=} type - The expected type of the argument.
      * @param {String=} typeDescription - The description of the expected type (used for formatting an error message).
      */
     argumentIsOptional(variable, variableName, type, typeDescription, predicate, predicateDescription) {
@@ -5239,7 +5239,7 @@ module.exports = (() => {
      *
      * @static
      * @param {*} variable - The value to check.
-     * @param {String} variableName - The name of the value (used for formatting an error message).
+     * @param {String=} variableName - The name of the value (used for formatting an error message).
      * @param {Function=} predicate - A function used to validate the item (beyond type checking).
      * @param {String=} predicateDescription - A description of the assertion made by the predicate (e.g. "is an integer") that is used for formatting an error message.
      */
