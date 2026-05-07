2ba637ba71c4daaaa130074981f803907db046c7
Luka Sotra <lukasotra@gmail.com>
2026-05-07 18:50:11 +0200

    refactor: sort currencies alphabetically

--- lang/Currency.js
+++ lang/Currency.js
@@ -61,6 +61,17 @@ module.exports = (() => {
 		}
 
 		/**
+		 * The Argentine Peso.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get ARS() {
+			return ars;
+		}
+
+		/**
 		 * The Australian Dollar.
 		 *
 		 * @public
@@ -69,6 +80,39 @@ module.exports = (() => {
 		 */
 		static get AUD() {
 			return aud;
+		}
+
+		/**
+		 * The Bermudian Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get BMD() {
+			return bmd;
+		}
+
+		/**
+		 * The Brazilian Real.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get BRL() {
+			return brl;
+		}
+
+		/**
+		 * The Bahamian Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get BSD() {
+			return bsd;
 		}
 
 		/**
@@ -94,6 +138,39 @@ module.exports = (() => {
 		}
 
 		/**
+		 * The Chinese Yuan.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get CNY() {
+			return cny;
+		}
+
+		/**
+		 * The Czech Koruna.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get CZK() {
+			return czk;
+		}
+
+		/**
+		 * The Danish Krone.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get DKK() {
+			return dkk;
+		}
+
+		/**
 		 * The Euro.
 		 *
 		 * @public
@@ -102,6 +179,17 @@ module.exports = (() => {
 		 */
 		static get EUR() {
 			return eur;
+		}
+
+		/**
+		 * The Fijian Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get FJD() {
+			return fjd;
 		}
 
 		/**
@@ -127,6 +215,17 @@ module.exports = (() => {
 		}
 
 		/**
+		 * The Ghanaian Cedi.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get GHS() {
+			return ghs;
+		}
+
+		/**
 		 * The Hong Kong Dollar.
 		 *
 		 * @public
@@ -135,259 +234,6 @@ module.exports = (() => {
 		 */
 		static get HKD() {
 			return hkd;
-		}
-
-		/**
-		 * The Japanese Yen.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get JPY() {
-			return jpy;
-		}
-
-		/**
-		 * The Norwegian Krone.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get NOK() {
-			return nok;
-		}
-
-		/**
-		 * The Swedish Krona.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get SEK() {
-			return sek;
-		}
-
-		/**
-		 * The US Dollar.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get USD() {
-			return usd;
-		}
-
-		/**
-		 * The Philippine peso.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get PHP() {
-			return php;
-		}
-
-		/**
-		 * The Singapore Dollar.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get SGD() {
-			return sgd;
-		}
-
-		/**
-		 * The New Zealand Dollar.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get NZD() {
-			return nzd;
-		}
-
-		/**
-		 * The Thai Baht.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get THB() {
-			return thb;
-		}
-
-		/**
-		 * The Mexican Peso.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get MXN() {
-			return mxn;
-		}
-
-		/**
-		 * The South African Rand.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get ZAR() {
-			return zar;
-		}
-
-		/**
-		 * The Malaysian Ringgit.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get MYR() {
-			return myr;
-		}
-
-		/**
-		 * The Israeli New Shekel.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get ILS() {
-			return ils;
-		}
-
-		/**
-		 * The Indonesian Rupiah.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get IDR() {
-			return idr;
-		}
-
-		/**
-		 * The Chinese Yuan.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get CNY() {
-			return cny;
-		}
-
-		/**
-		 * The Danish Krone.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get DKK() {
-			return dkk;
-		}
-
-		/**
-		 * The Polish Zloty.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get PLN() {
-			return pln;
-		}
-
-		/**
-		 * The Turkish Lira.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get TRY() {
-			return trx;
-		}
-
-		/**
-		 * The Peruvian Sol.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get PEN() {
-			return pen;
-		}
-
-		/**
-		 * The South Korean Won.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get KRW() {
-			return krw;
-		}
-
-		/**
-		 * The Czech Koruna.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get CZK() {
-			return czk;
-		}
-
-		/**
-		 * The Brazilian Real.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get BRL() {
-			return brl;
-		}
-
-		/**
-		 * The Papua New Guinean Kina.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get PGK() {
-			return pgk;
-		}
-
-		/**
-		 * The Russian Ruble.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get RUB() {
-			return rub;
 		}
 
 		/**
@@ -402,58 +248,25 @@ module.exports = (() => {
 		}
 
 		/**
-		 * The New Taiwan Dollar.
+		 * The Indonesian Rupiah.
 		 *
 		 * @public
 		 * @static
 		 * @returns {Currency}
 		 */
-		static get TWD() {
-			return twd;
+		static get IDR() {
+			return idr;
 		}
 
 		/**
-		 * The Nigerian Naira.
+		 * The Israeli New Shekel.
 		 *
 		 * @public
 		 * @static
 		 * @returns {Currency}
 		 */
-		static get NGN() {
-			return ngn;
-		}
-
-		/**
-		 * The Argentine Peso.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get ARS() {
-			return ars;
-		}
-
-		/**
-		 * The Bermudian Dollar.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get BMD() {
-			return bmd;
-		}
-
-		/**
-		 * The Fijian Dollar.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get FJD() {
-			return fjd;
+		static get ILS() {
+			return ils;
 		}
 
 		/**
@@ -468,58 +281,25 @@ module.exports = (() => {
 		}
 
 		/**
-		 * The Ghanaian Cedi.
+		 * The Japanese Yen.
 		 *
 		 * @public
 		 * @static
 		 * @returns {Currency}
 		 */
-		static get GHS() {
-			return ghs;
+		static get JPY() {
+			return jpy;
 		}
 
 		/**
-		 * The Russian Ruble (Old).
+		 * The South Korean Won.
 		 *
 		 * @public
 		 * @static
 		 * @returns {Currency}
 		 */
-		static get RUR() {
-			return rur;
-		}
-
-		/**
-		 * The Uruguay Peso.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get UYI() {
-			return uyi;
-		}
-
-		/**
-		 * The Namibian Dollar.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get NAD() {
-			return nad;
-		}
-
-		/**
-		 * The Zambian Kwacha.
-		 *
-		 * @public
-		 * @static
-		 * @returns {Currency}
-		 */
-		static get ZMW() {
-			return zmw;
+		static get KRW() {
+			return krw;
 		}
 
 		/**
@@ -534,14 +314,234 @@ module.exports = (() => {
 		}
 
 		/**
-		 * The Bahamian Dollar.
+		 * The Mexican Peso.
 		 *
 		 * @public
 		 * @static
 		 * @returns {Currency}
 		 */
-		static get BSD() {
-			return bsd;
+		static get MXN() {
+			return mxn;
+		}
+
+		/**
+		 * The Malaysian Ringgit.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get MYR() {
+			return myr;
+		}
+
+		/**
+		 * The Namibian Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get NAD() {
+			return nad;
+		}
+
+		/**
+		 * The Nigerian Naira.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get NGN() {
+			return ngn;
+		}
+
+		/**
+		 * The Norwegian Krone.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get NOK() {
+			return nok;
+		}
+
+		/**
+		 * The New Zealand Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get NZD() {
+			return nzd;
+		}
+
+		/**
+		 * The Peruvian Sol.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get PEN() {
+			return pen;
+		}
+
+		/**
+		 * The Papua New Guinean Kina.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get PGK() {
+			return pgk;
+		}
+
+		/**
+		 * The Philippine peso.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get PHP() {
+			return php;
+		}
+
+		/**
+		 * The Polish Zloty.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get PLN() {
+			return pln;
+		}
+
+		/**
+		 * The Russian Ruble.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get RUB() {
+			return rub;
+		}
+
+		/**
+		 * The Russian Ruble (Old).
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get RUR() {
+			return rur;
+		}
+
+		/**
+		 * The Swedish Krona.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get SEK() {
+			return sek;
+		}
+
+		/**
+		 * The Singapore Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get SGD() {
+			return sgd;
+		}
+
+		/**
+		 * The Thai Baht.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get THB() {
+			return thb;
+		}
+
+		/**
+		 * The Turkish Lira.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get TRY() {
+			return trx;
+		}
+
+		/**
+		 * The New Taiwan Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get TWD() {
+			return twd;
+		}
+
+		/**
+		 * The US Dollar.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get USD() {
+			return usd;
+		}
+
+		/**
+		 * The Uruguay Peso.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get UYI() {
+			return uyi;
+		}
+
+		/**
+		 * The South African Rand.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get ZAR() {
+			return zar;
+		}
+
+		/**
+		 * The Zambian Kwacha.
+		 *
+		 * @public
+		 * @static
+		 * @returns {Currency}
+		 */
+		static get ZMW() {
+			return zmw;
 		}
 
 		toString() {
@@ -549,50 +549,50 @@ module.exports = (() => {
 		}
 	}
 
+	const ars = new Currency('ARS', 'Argentine Peso', 2, 'ARS');
 	const aud = new Currency('AUD', 'Australian Dollar', 2, 'AUD$');
+	const bmd = new Currency('BMD', 'Bermudian Dollar', 2, 'BMD');
+	const brl = new Currency('BRL', 'Brazilian Real', 2, 'BRL');
+	const bsd = new Currency('BSD', 'Bahamian Dollar', 2, 'BSD');
 	const cad = new Currency('CAD', 'Canadian Dollar', 2, 'CAD$');
 	const chf = new Currency('CHF', 'Swiss Franc', 2, 'CHF');
+	const cny = new Currency('CNY', 'Chinese Yuan', 2, 'CNY');
+	const czk = new Currency('CZK', 'Czech Koruna', 2, 'CZK');
+	const dkk = new Currency('DKK', 'Danish Krone', 2, 'DKK');
 	const eur = new Currency('EUR', 'Euro', 2, 'EUR');
+	const fjd = new Currency('FJD', 'Fijian Dollar', 2, 'FJD');
 	const gbp = new Currency('GBP', 'British Pound', 2, 'GBP');
 	const gbx = new Currency('GBX', 'British Penny', 2, 'GBX');
-	const hkd = new Currency('HKD', 'Hong Kong Dollar', 2, 'HK$');
-	const jpy = new Currency('JPY', 'Japanese Yen', 2, 'JPY');
-	const nok = new Currency('NOK', 'Norwegian Krone', 2, 'Nkr');
-	const sek = new Currency('SEK', 'Swedish Krona', 2, 'SEK');
-	const usd = new Currency('USD', 'US Dollar', 2, 'US$');
-	const php = new Currency('PHP', 'Philippine peso', 2, 'PHP');
-	const sgd = new Currency('SGD', 'Singapore Dollar', 2, 'SGD');
-	const nzd = new Currency('NZD', 'New Zealand Dollar', 2, 'NZD');
-	const thb = new Currency('THB', 'Thai Baht', 2, 'THB');
-	const mxn = new Currency('MXN', 'Mexican Peso', 2, 'MXN');
-	const zar = new Currency('ZAR', 'South African Rand', 2, 'ZAR');
-	const myr = new Currency('MYR', 'Malaysian Ringgit', 2, 'MYR');
-	const ils = new Currency('ILS', 'Israeli New Shekel', 2, 'ILS');
-	const idr = new Currency('IDR', 'Indonesian Rupiah', 2, 'IDR');
-	const cny = new Currency('CNY', 'Chinese Yuan', 2, 'CNY');
-	const dkk = new Currency('DKK', 'Danish Krone', 2, 'DKK');
-	const pln = new Currency('PLN', 'Polish Zloty', 2, 'PLN');
-	const trx = new Currency('TRY', 'Turkish Lira', 2, 'TRY');
-	const pen = new Currency('PEN', 'Peruvian Sol', 2, 'PEN');
-	const krw = new Currency('KRW', 'South Korean Won', 2, 'KRW');
-	const czk = new Currency('CZK', 'Czech Koruna', 2, 'CZK');
-	const brl = new Currency('BRL', 'Brazilian Real', 2, 'BRL');
-	const pgk = new Currency('PGK', 'Papua New Guinean Kina', 2, 'PGK');
-	const rub = new Currency('RUB', 'Russian Ruble', 2, 'RUB');
-	const huf = new Currency('HUF', 'Hungarian Forint', 2, 'HUF');
-	const twd = new Currency('TWD', 'New Taiwan Dollar', 2, 'TWD');
-	const ngn = new Currency('NGN', 'Nigerian Naira', 2, 'NGN');
-	const ars = new Currency('ARS', 'Argentine Peso', 2, 'ARS');
-	const bmd = new Currency('BMD', 'Bermudian Dollar', 2, 'BMD');
-	const fjd = new Currency('FJD', 'Fijian Dollar', 2, 'FJD');
-	const jod = new Currency('JOD', 'Jordanian Dinar', 2, 'JOD');
 	const ghs = new Currency('GHS', 'Ghanaian Cedi', 2, 'GHS');
-	const rur = new Currency('RUR', 'Russian Ruble (Old)', 2, 'RUR');
-	const uyi = new Currency('UYI', 'Uruguay Peso', 2, 'UYI');
-	const nad = new Currency('NAD', 'Namibian Dollar', 2, 'NAD');
-	const zmw = new Currency('ZMW', 'Zambian Kwacha', 2, 'ZMW');
+	const hkd = new Currency('HKD', 'Hong Kong Dollar', 2, 'HK$');
+	const huf = new Currency('HUF', 'Hungarian Forint', 2, 'HUF');
+	const idr = new Currency('IDR', 'Indonesian Rupiah', 2, 'IDR');
+	const ils = new Currency('ILS', 'Israeli New Shekel', 2, 'ILS');
+	const jod = new Currency('JOD', 'Jordanian Dinar', 2, 'JOD');
+	const jpy = new Currency('JPY', 'Japanese Yen', 2, 'JPY');
+	const krw = new Currency('KRW', 'South Korean Won', 2, 'KRW');
 	const lbp = new Currency('LBP', 'Lebanese Pound', 2, 'LBP');
-	const bsd = new Currency('BSD', 'Bahamian Dollar', 2, 'BSD');
+	const mxn = new Currency('MXN', 'Mexican Peso', 2, 'MXN');
+	const myr = new Currency('MYR', 'Malaysian Ringgit', 2, 'MYR');
+	const nad = new Currency('NAD', 'Namibian Dollar', 2, 'NAD');
+	const ngn = new Currency('NGN', 'Nigerian Naira', 2, 'NGN');
+	const nok = new Currency('NOK', 'Norwegian Krone', 2, 'Nkr');
+	const nzd = new Currency('NZD', 'New Zealand Dollar', 2, 'NZD');
+	const pen = new Currency('PEN', 'Peruvian Sol', 2, 'PEN');
+	const pgk = new Currency('PGK', 'Papua New Guinean Kina', 2, 'PGK');
+	const php = new Currency('PHP', 'Philippine peso', 2, 'PHP');
+	const pln = new Currency('PLN', 'Polish Zloty', 2, 'PLN');
+	const rub = new Currency('RUB', 'Russian Ruble', 2, 'RUB');
+	const rur = new Currency('RUR', 'Russian Ruble (Old)', 2, 'RUR');
+	const sek = new Currency('SEK', 'Swedish Krona', 2, 'SEK');
+	const sgd = new Currency('SGD', 'Singapore Dollar', 2, 'SGD');
+	const thb = new Currency('THB', 'Thai Baht', 2, 'THB');
+	const trx = new Currency('TRY', 'Turkish Lira', 2, 'TRY');
+	const twd = new Currency('TWD', 'New Taiwan Dollar', 2, 'TWD');
+	const usd = new Currency('USD', 'US Dollar', 2, 'US$');
+	const uyi = new Currency('UYI', 'Uruguay Peso', 2, 'UYI');
+	const zar = new Currency('ZAR', 'South African Rand', 2, 'ZAR');
+	const zmw = new Currency('ZMW', 'Zambian Kwacha', 2, 'ZMW');
 
 	return Currency;
 })();
