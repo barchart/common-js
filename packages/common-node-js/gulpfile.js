e1671abe3c45e9bc4114e9963b307c0d8119d74c
Luka Sotra <lukasotra@gmail.com>
2026-06-16 00:28:41 +0200

    chore: update several configuration file, prepare for changes from CommonJS to ESM modules

--- packages/common-node-js/gulpfile.js
+++ packages/common-node-js/gulpfile.js
@@ -1,26 +1,30 @@
-const exec = require('child_process').exec;
+import { exec } from 'node:child_process';
+import fs from 'node:fs';
 
-const gulp = require('gulp');
+import git from 'gulp-git';
+import gitStatus from 'git-get-status';
+import gulp from 'gulp';
+import jshint from 'gulp-jshint';
+import prompt from 'gulp-prompt';
 
-const git = require('gulp-git'),
-    gitStatus = require('git-get-status'),
-    jshint = require('gulp-jshint'),
-	prompt = require('gulp-prompt');
-
-const fs = require('fs');
+let bump;
 
 function getVersionFromPackage() {
-    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+	return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
 }
 
 gulp.task('ensure-clean-working-directory', (cb) => {
-    gitStatus((err, status) => {
-        if (err, !status.clean) {
-            throw new Error('Unable to proceed, your working directory is not clean.');
-        }
+	gitStatus((error, status) => {
+		if (error) {
+			return cb(error);
+		}
 
-        cb();
-    });
+		if (!status.clean) {
+			return cb(new Error('Unable to proceed, your working directory is not clean.'));
+		}
+
+		cb();
+	});
 });
 
 gulp.task('bump-choice', (cb) => {
@@ -29,8 +33,8 @@ gulp.task('bump-choice', (cb) => {
 		name: 'bump',
 		message: 'What type of bump would you like to do?',
 		choices: ['patch', 'minor', 'major'],
-	}, (res) => {
-		global.bump = res.bump;
+	}, (result) => {
+		bump = result.bump;
 
 		return cb();
 	});
@@ -39,11 +43,11 @@ gulp.task('bump-choice', (cb) => {
 });
 
 gulp.task('bump-version', (cb) => {
-	exec(`npm version ${global.bump || 'patch'} --no-git-tag-version`, {
+	exec(`npm version ${bump || 'patch'} --no-git-tag-version`, {
 		cwd: './'
 	}, (error) => {
 		if (error) {
-			cb(error);
+			return cb(error);
 		}
 
 		cb();
@@ -51,31 +55,31 @@ gulp.task('bump-version', (cb) => {
 });
 
 gulp.task('commit-changes', () => {
-    return gulp.src([ './', './package.json' ])
-        .pipe(git.add())
-        .pipe(git.commit('Release. Bump version number'));
+	return gulp.src([ './', './package.json' ])
+		.pipe(git.add())
+		.pipe(git.commit('Release. Bump version number'));
 });
 
 gulp.task('push-changes', (cb) => {
-    git.push('origin', 'master', cb);
+	git.push('origin', 'master', cb);
 });
 
 gulp.task('create-tag', (cb) => {
-    const version = getVersionFromPackage();
+	const version = getVersionFromPackage();
 
-    git.tag(version, 'Release ' + version, (error) => {
-        if (error) {
-            return cb(error);
-        }
+	git.tag(version, 'Release ' + version, (error) => {
+		if (error) {
+			return cb(error);
+		}
 
-        git.push('origin', 'master', { args: '--tags' }, cb);
-    });
+		git.push('origin', 'master', { args: '--tags' }, cb);
+	});
 });
 
 gulp.task('execute-node-tests', (cb) => {
-	exec(`npm test`, { cwd: './' }, (error) => {
+	exec('npm test', { cwd: './' }, (error) => {
 		if (error) {
-			cb(error);
+			return cb(error);
 		}
 
 		cb();
@@ -91,12 +95,12 @@ gulp.task('release', gulp.series(
 	'bump-version',
 	'commit-changes',
 	'push-changes',
-	'create-tag'
-));
+	'create-tag')
+);
 
 gulp.task('lint', () => {
 	return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./test/dist/**' ])
-		.pipe(jshint({ esversion: 9 }))
+		.pipe(jshint({ esversion: 11, module: true, node: true, browser: true, jasmine: true, '-W079': true, '-W117': true }))
 		.pipe(jshint.reporter('default'))
 		.pipe(jshint.reporter('fail'));
 });
